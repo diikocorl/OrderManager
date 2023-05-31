11dd8c0d3ffb370a424a8d40a1a2acd4c1659485
diikocorl <nkhoi3258@gmail.com>
2023-05-31 11:11:02 +0700

    up

--- App/Order/deployment/native/styles.js
+++ App/Order/deployment/native/styles.js
@@ -1,9 +1,9 @@
-import * as style0 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderMProject/App/Order/themesource/administration/native/main";
-import * as style1 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderMProject/App/Order/themesource/nanoflowcommons/native/main";
-import * as style2 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderMProject/App/Order/themesource/webactions/native/main";
-import * as style3 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderMProject/App/Order/themesource/atlas_core/native/main";
-import * as style4 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderMProject/App/Order/themesource/ordermanage/native/main";
-import * as style5 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderMProject/App/Order/theme/native/main";
+import * as style0 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderManager/App/Order/themesource/administration/native/main";
+import * as style1 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderManager/App/Order/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderManager/App/Order/themesource/webactions/native/main";
+import * as style3 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderManager/App/Order/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderManager/App/Order/themesource/ordermanage/native/main";
+import * as style5 from "C:/Users/nkhoi/Documents/GITHUB/Project/OrderManager/App/Order/theme/native/main";
 
 import { flatten } from "mendix/native";
 
